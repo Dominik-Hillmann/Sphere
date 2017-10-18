ff8d8eabe52b200515fda7314c1788279350ad49
Dominik-Hillmann <dominik.hillmann@gmx.de>
2017-10-18 22:40:44 +0200

    Add files via upload

--- sphere.functions.js
+++ sphere.functions.js
@@ -3,5 +3,17 @@ var haveDistance = function(x, y, middlePoint, dist)
 {
    var actualDist = Math.round(Math.sqrt(Math.pow(x - middlePoint.x, 2) + Math.pow(y - middlePoint.y, 2)));
    //console.log(actualDist);
-   return actualDist === dist || actualDist + 1 === dist || actualDist - 1 === dist;//(dist >= actualDist - 1) && (dist <= actualDist + 1);
+   return ((actualDist == dist) || ((actualDist + 2) == dist) || ((actualDist - 2) == dist));//(dist >= actualDist - 1) && (dist <= actualDist + 1);
+}
+
+var reDistance = function(x, y, middlePos)
+{
+   return Math.round(Math.sqrt(Math.pow(x - middlePoint.x, 2) + Math.pow(y - middlePoint.y, 2)));
+}
+
+// creates new vector
+var Position = function(x, y)
+{
+   this.x = x;
+   this.y = y;
 }
--- sphere.sketch.js
+++ sphere.sketch.js
@@ -1,33 +1,52 @@
-const SPHERE_RADIUS = 100;
+const SPHERE_RADIUS = 200;
 const WIDTH = 300;
 const HEIGHT = 300;
 
-var circlePoints = [];
-var middlePoint =
-{
-   x : WIDTH / 2,
-   y : HEIGHT / 2
-}
+var circlePositions = [];
+var circleDists = [];
+var middlePoint = new Position(WIDTH / 2, HEIGHT / 2);
+
 
 function setup()
 {
    var canvas = createCanvas(WIDTH, HEIGHT);
-   console.log(middlePoint.x, middlePoint.y);
 
    // getting all points that make a cirlce with RADIUS
-   for(var x = 1; x < WIDTH; x++)
+   /*for(var x = 1; x <= WIDTH; x++)
    {
-      for(var y = 1; y < HEIGHT; y++)
+      for(var y = 1; y <= HEIGHT; y++)
       {
          if(haveDistance(x, y, middlePoint, RADIUS))
          {
-            circlePoints.push(new Vector(x, y));
+            circlePoints.push(new Position(x, y));
             console.log("Geschafft: ", circlePoints[circlePoints.length]);
          }
       }
+   }*/
+
+   // einfach jeden Punkt mit Abstand reinmachen
+
+   for(var x = 1; x <= WIDTH; x++)
+   {
+      for(var y = 1; y <= HEIGHT; y++)
+      {
+         circlePositions.push(new Position(x, y));
+         circleDists.push(reDistance(x, y, middlePoint));
+      }
    }
 
-   console.log(circlePoints);
+   console.log("Dists", circleDists);
+   console.log("pos", circlePositions);
+   var filteredPositions = [];
+   for(var i = 0; i < circlePositions.length; i++)
+   {
+      if(circleDists[i] == SPHERE_RADIUS)
+         filteredPositions.push(circlePositions[i]);
+   }
+   console.log("Filtered", filteredPositions);
+   cirlcePositions = filteredPositions;
+   // circlePoints.push("LALALALA");
+   console.log(circlePositions);
 }
 
 function draw()
@@ -35,7 +54,8 @@ function draw()
    background(0, 0, 0);
 
 
-   ellipse(middlePoint.x, middlePoint.y, 4);
+   ellipse(middlePoint.x, middlePoint.y, SPHERE_RADIUS);
+
    // finding a way to put dots into a circle
 
 }
